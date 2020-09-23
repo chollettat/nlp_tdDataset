727a8da9f504892ac7265f5577d10e6dadb09879
Tatiana Chollet <chollettat@eisti.eu>
2020-09-23 15:05:00 +0200

    split donnees

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+#imports
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-X = pd.read_csv('smsspamcollection.txt', sep="\t", header=None)
-print(X[1])
+#read the file, split in 2 columns (ham/spam and comments)
+datas = pd.read_csv('smsspamcollection.txt', sep="\t", header=None)
+#datas.columns = ["type","comments"]
+print(datas[1])
+
+#split dataset 
+train, test = train_test_split(datas, test_size=0.2, random_state=42, shuffle=True)
