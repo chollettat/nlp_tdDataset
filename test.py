b43e2533a7d28eb0908aa98441d827dd695a7999
Tatiana Chollet <chollettat@eisti.eu>
2020-09-23 19:01:44 +0200

    convert to csv, error type for string

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 datas = pd.read_csv('smsspamcollection.txt', sep="\t", header=None,converters={i: str for i in range(0,2)})
 datas.to_csv('smsspamcollection.csv',index=None)
 datas[0] = datas[0].map({'ham':0,'spam':1})
-print(datas)
+datas = datas.applymap(str)
+datas = datas.astype(str)
+print((datas))
 #split dataset 
 train, test = train_test_split(datas, test_size=0.2, random_state=42, shuffle=True)
