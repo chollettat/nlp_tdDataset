a91d67ef3bb470db8a84cfca02683bd4d5c98cda
Tatiana Chollet <chollettat@eisti.eu>
2020-09-26 16:23:20 +0200

    Comments stopwords

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,7 @@
 # imporations
 import re
-from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize 
+#from nltk.corpus import stopwords 
+#from nltk.tokenize import word_tokenize 
 
 # read and split txt file in several lines
 lines = tuple(open('smsspamcollection.txt', 'r'))
@@ -14,9 +14,10 @@ for i in range(0,len(lines)) :
     res[i][1] = res[i][1].lower()
 
     # remove stopwords
-    stop_words = set(stopwords.words('english'))
-    tokens = word_tokenize(res[i][1])
-    res[i][1] = [i for i in tokens if not i in stop_words]
+    #stop_words = set(stopwords.words('english'))
+    #tokens = word_tokenize(res[i][1])
+    #res[i][1] = [i for i in tokens if not i in stop_words]
+
 
     ###TESTS FOR FIND ANOTHER STOPWORDS###
     if res[i][0]=='spam':
@@ -26,9 +27,10 @@ for i in range(0,len(lines)) :
             nbSpamNumber+=1
     else : 
         nbHam+=1
+        print(type(res[i][1]))
         test=bool(re.search(r'\d',res[i][1]))
         if test==True :
             nbHamNumber+=1
             #res[i][1] = re.sub(r'\d+','',res[i][1])
-    print(res[i][1])
+    #print(res[i][1])
 
