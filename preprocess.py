4c9dfa0b1cbe331166295ffece777e7e64446001
Tatiana Chollet <chollettat@eisti.eu>
2020-09-27 22:30:27 +0200

    end of stemming + corr on spamcollection

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 # imporations
 import re
+import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize 
 from nltk.stem import PorterStemmer
@@ -13,27 +14,31 @@ lines = tuple(open('smsspamcollection.txt', 'r'))
 # def variables
 res, stemList, hamList, spamList = [], [], [], []
 
-#preprocessing on spamcollection
+# preprocessing on spamcollection
 def preprocess(lines,res,stemList) : 
 
     for i in range(0,len(lines)) :
-        res.append(lines[i].split("\t")) #split with ham/spam and message
-        res[i][1] = res[i][1].lower().strip()
+        res.append(lines[i].split("\t")) # split with ham/spam and message
+        res[i][1] = res[i][1].lower().strip().translate(str.maketrans("","",string.punctuation)) # convert to lowercase and remove with spaces
+        res[i][1] = re.sub(r'\d+','',res[i][1]) # remove numbers
 
-    ##define stopwords##
+    ## define stopwords##
     stop_words = stopwords.words('english') 
-    stop_words.append("u") #add stopwords to the list
+    stop_words.append("u") # add stopwords to the list
     stemmer = PorterStemmer()
 
     for i in range(0,len(lines)) :
         tokens = word_tokenize(res[i][1])
+        stemList=[]
         res[i][1] = [word for word in tokens if not word in stop_words]   # remove stopwords
-        for word in res[i][1]:
-            stemList.append(stemmer.stem(word))
+        for word in res[i][1] :
+            if len(word) > 2 : # remove words with 2 or less caracters
+                stemList.append(stemmer.stem(word))
+        res[i][1] = stemList
     return res
 
 
-#split res into 2 lists, 1 for spam, 1 for ham
+# split res into 2 lists, 1 for spam, 1 for ham
 def splitList(lines,res,spamList,hamList) :
     res = preprocess(lines,res,stemList)
     for i in range(0,len(lines)) :
@@ -44,5 +49,4 @@ def splitList(lines,res,spamList,hamList) :
     return hamList, spamList
 
 hamList,spamList = splitList(lines,res,spamList,hamList)
-print(hamList)
 
