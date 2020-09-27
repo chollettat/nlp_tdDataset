72b4256c70f3125b12a99f20aba3433c5dbf2d11
Tatiana Chollet <chollettat@eisti.eu>
2020-09-27 20:28:32 +0200

    functions for preprocess and split list

--- preprocess.py
+++ preprocess.py
@@ -1,36 +1,47 @@
 # imporations
 import re
-from nltk.corpus import stopwords 
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize 
-from nltk import stem
+from nltk.stem import PorterStemmer
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('stopwords')
 
-# read and split txt file in several lines
+# read txt file in several lines
 lines = tuple(open('smsspamcollection.txt', 'r'))
-res, hamList, spamList = [], [],[]
 
-for i in range(0,len(lines)) :
-    res.append(lines[i].split("\t"))
-    res[i][1] = res[i][1].lower()
+# def variables
+res, hamList, spamList = [], [], []
 
-    ###TESTS FOR FIND ANOTHER STOPWORDS###
-    if res[i][0]=='spam':
-        spamList.append(res[i])
-    else : 
-       hamList.append(res[i])
+#preprocessing on spamcollection
+def preprocess(lines,res) : 
 
-#res[i][1] = re.sub(r'\d+','',res[i][1])
-#stemmer = stem.SnowballStemmer('english')
-stop_words = stopwords.words('english')
-#stop_words.append("go")
+    for i in range(0,len(lines)) :
+        res.append(lines[i].split("\t")) #split with ham/spam and message
+        res[i][1] = res[i][1].lower().strip()
 
-for i in range(0,len(lines)) :
+    ##define stopwords##
+    stop_words = stopwords.words('english') 
+    #stop_words.append("ok")
+    stemmer = PorterStemmer()
 
-  # remove stopwords
-  tokens = word_tokenize(res[i][1])
-  res[i][1] = [word for word in tokens if not word in stop_words]
+    for i in range(0,len(lines)) :
+        tokens = word_tokenize(res[i][1])
+        res[i][1] = [word for word in tokens if not word in stop_words]   # remove stopwords
+        #for word in res[i][1]:
+            #res[i][1] = stemmer.stem(word)
+    return res
 
 
+def splitList(lines,res,spamList,hamList) :
+    res = preprocess(lines,res)
+    for i in range(0,len(lines)) :
+        if res[i][0]=='spam':
+            spamList.append(res[i])
+        else : 
+            hamList.append(res[i])
+    return hamList, spamList
+
+hamList,spamList = splitList(lines,res,spamList,hamList)
 print(hamList)
+
